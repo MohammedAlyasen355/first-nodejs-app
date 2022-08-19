9d23eba6052d58f5c9f84ccd55718404db17a073
Mohammed Al-yaseen <mouhamadalyasen3555@gmail.com>
2022-08-19 07:59:31 +0300

    Using env variable

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send([
-    1,'HMODEE'  
+    1,'MODE'  
   ])
 })
 
