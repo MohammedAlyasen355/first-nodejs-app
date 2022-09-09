5bd146cbe10016be5513a683373e1710116578d6
Mohammed Al-yaseen <mouhamadalyasen3555@gmail.com>
2022-09-09 15:31:44 +0300

    Extract category schema

--- models/category.js
+++ models/category.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-const Category = mongoose.model(
-  "Category",
-  new mongoose.Schema({
-    name: { type: String, required: true, minlength: 5, maxLength: 255 },
-    phone: { type: String, minlength: 5, maxLength: 255 },
-  })
-);
+const categorySchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 5, maxLength: 255 },
+  phone: { type: String, minlength: 5, maxLength: 255 },
+});
+
+const Category = mongoose.model("Category", categorySchema);
 
 exports.Category = Category;
+exports.categorySchema = categorySchema;
