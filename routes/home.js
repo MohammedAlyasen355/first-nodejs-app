7e4193332694744d2ccae068431b969a8eba2224
Mohammed Al-yaseen <mouhamadalyasen3555@gmail.com>
2023-05-27 11:58:52 +0300

    Working on welcome page

--- routes/home.js
+++ routes/home.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send(courses);
-  // res.render("index", { title: "TuhMoh", message: "You are welcome" });
+  res.render("index", {
+    title: "Steam-AIU",
+    message: `You are welcome .. Node app is working on port 
+      ${process.env.PORT || 3000}`,
+  });
 });
 
 module.exports = router;
